6660de1bd43141712d8ed5b8e20aa8d8b5be565f
Yaroslav Grogul <time7bass@gmail.com>
2023-01-04 13:59:53 +0100

    Initial Commit

---
+++ Plion/PlionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlionApp.swift
+//  Plion
+//
+//  Created by Ярослав Грогуль on 04.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PlionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
